7f97cca8d86a8eea6e5355ab320d03a9dbf08f22
Abena Wiafe <inna.wiafe@gmail.com>
2022-06-25 19:31:32 +0000

    client side code changes

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ document.querySelector('button').addEventListener('click', apiRequest)
 async function apiRequest(){
     try{
         const movieGenre = document.querySelector('input').value;
-        const response = await fetch(`https://localhost:8080/api/${movieGenre}`);
+        const response = await fetch(`https://a-movie-quotes-api.herokuapp.com/api/${movieGenre}`);
         const data = await response.json();
 
         console.log(data)
